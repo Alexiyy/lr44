303d3441b7102c0e80ca8a0af79934901655edde
Alexiyy <98177448+Alexiyy@users.noreply.github.com>
2022-12-12 11:07:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+dictionary = {}
+
+
+def input_path():
+    pass  # 
+
+
+def create_dict(path):
+    pass  # 
+
+
+def analyze_dict(dictionary):
+    pass  # 
+
+
+def duplicate_files(doubles):
+    pass #
+
+if __name__ == '__main__':
+    duplicate_files(analyze_dict(create_dict(input_path())))
